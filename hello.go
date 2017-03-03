ddb7ad1937b123c756b7f2fa50c6eb2e25bc4997
HamzaAnis <hamzaanis9514@gmail.com>
2017-03-03 22:20:50 +0500

    Correcting last commit

--- hello.go
+++ hello.go
@@ -2,9 +2,9 @@ package main
 
 import "fmt"
 
-func Myfunc()
-{
-	fmt.Pritnln("Checking package public func")	
+//Myfunc simply checks the package functonality.
+func MyFunc() {
+	fmt.Println("Checking package public func")
 }
 func main() {
 	fmt.Printf("hello, world\n")
